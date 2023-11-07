1a3da10416a02a85c23b5b7c4ba876c13783dd80
BayuTriwibowo18 <144087130+BayuTriwibowo18@users.noreply.github.com>
2023-11-07 11:03:30 +0700

    Add files via upload

---
+++ Star07.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Star07 {
+    public static void main(String[] args) {
+        Scanner input07 =new Scanner(System.in);
+
+        System.out.println("Masukkan nilai N");
+        int N =input07.nextInt();
+
+        for(int i =1; i<=N;i++){
+            System.out.print("*");
+        
+    }
+}
+}
